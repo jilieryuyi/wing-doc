57026786e4c96e9bbe0bd3aa1464c0eb1d945165
xiaoan <huangxiaoan@yonglibao.com>
2017-06-16 16:45:24 +0800

    update demo

--- demo/test.php
+++ demo/test.php
@@ -14,20 +14,24 @@ function get_millisecond()
     return (float)sprintf('%.0f', (floatval($time[0]) + floatval($time[1])) * 1000);
 }
 
-$start_time = get_millisecond();
+try {
+    $start_time = get_millisecond();
 
-$app = new \Wing\Doc\Doc(
-    "/Users/yuyi/Web/xiaoan/api",
-    "/Users/yuyi/Web/xiaoan/wing/doc"
-);
-$app->addExcludePath([
-    "vendor/*","Config/*","config/*",
-    "public/*","database/*","tests/*"
-]);
-$app->addExcludeFileName([
-    "artisan","composer","app.php","web.php"
-]);
-$app->run();
+    $app = new \Wing\Doc\Doc(
+        "/Users/yuyi/Web/xiaoan/api",
+        "/Users/yuyi/Web/xiaoan/wing/doc"
+    );
+    $app->addExcludePath([
+        "vendor/*", "Config/*", "config/*",
+        "public/*", "database/*", "tests/*"
+    ]);
+    $app->addExcludeFileName([
+        "artisan", "composer", "app.php", "web.php"
+    ]);
+    $app->run();
 
 
-echo "完成，耗时".(get_millisecond()-$start_time)."毫秒\r\n";
+    echo "完成，耗时" . (get_millisecond() - $start_time) . "毫秒\r\n";
+} catch(\Exception $e) {
+    echo $e->getMessage();
+}
